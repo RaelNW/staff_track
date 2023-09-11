a9a99922e6ca23190fe09279d1faf854cb220abb
Rael Wanjala <raelnwanjala@gmail.com>
2023-09-11 00:12:45 -0700

    Added prompts on index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const inquirer = require("inquirer");
+
+function promptUser() {
+  inquirer
+    .prompt([
+  
+      {
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["View All Employees", "Add Employee", 
+        "View All Roles", "Add Role", 
+        "View All Departments", "Add Department", "Quit"],
+        name: "create",
+      },
+      {
+        type: "input",
+        message: "What is the name of the department?",
+        name: "departmentName",
+      },
+      {
+        type: "input",
+        message: "What is the name of the role?",
+        name: "roleName",
+      },
+      {
+        type: "input",
+        message: "What is the salary of the role?",
+        name: "roleSalary",
+      },
+      {
+        type: "input",
+        message: "What department does the role belong to?",
+        choices: ["Engineering","Finance","Legal","Sales","Service"]
+        name: "DepartmentName",
+      },
+      ])}
