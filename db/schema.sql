aad29955c5fe1eb91d61cbb1ad24426e836a5839
Rael Wanjala <raelnwanjala@gmail.com>
2023-09-12 03:32:21 -0700

    Updated prompts, created a displaydata function and a connection to the database

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL,
-  department_id INT, 
+  department_id INT,
   FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
@@ -25,4 +25,3 @@ CREATE TABLE employee (
   FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
   FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
-
