6b179fc1c89f51f0733f953cf4923e95722217fc
Rael Wanjala <raelnwanjala@gmail.com>
2023-09-12 17:10:27 -0700

    Addded function on index.js to add department when user selects it from the function

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const connection = mysql.createConnection({
   password: "password",
   database: "tracker_db",
 });
+
 connection.connect((err) => {
   if (err) {
-    console.error("Error connecting to the database: " + err.stack);
+    console.error("Error connecting to the database: " + err);
     return;
   }
   console.log(`Connected to the tracker_db database.`);
@@ -18,6 +19,7 @@ connection.connect((err) => {
   //start prompting users
   promptUser();
 });
+
 //Display database in relation to users choices
 function displayData(choice) {
   let query;
@@ -42,7 +44,7 @@ function displayData(choice) {
     });
   }
 }
-
+//Prompt users on what they would like to do with the database
 function promptUser() {
   inquirer
     .prompt([
@@ -64,6 +66,7 @@ function promptUser() {
 
     .then((answers) => {
       if (answers.action === "Quit") {
+        // if user selects quit end prompts
         connection.end();
         console.log("See you next time");
         return;
@@ -71,4 +74,32 @@ function promptUser() {
       displayData(answers.action);
     });
 }
-promptUser();
+
+//Function to add dept when user wants to
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter Department name",
+      },
+    ])
+    .then((answer) => {
+      connection.query(
+        `INSERT INTO department(name)
+                    VALUES(?)`,
+        answer.addDepartment,
+        (err, results) => {
+          if (err) {
+            console.log(err);
+          } else {
+            connection.query(`SELECT * FROM department`, (err, results) => {
+              err ? console.error(err) : console.table(results);
+              init();
+            });
+          }
+        }
+      );
+    });
+}
